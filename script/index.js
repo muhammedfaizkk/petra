666caedf5ec28531e585a7bbfa337fc43feffc58
Muhammed faiz kk <fayizkk5@gmail.com>
2024-04-11 05:16:21 +0530

    added testimonials

--- script/index.js
+++ script/index.js
@@ -12,4 +12,19 @@
     }
   });
 
- 
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  
+  
