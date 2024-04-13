c0a775d6bbc68e547f1b386885eeef79dc9bedc5
Muhammed faiz kk <fayizkk5@gmail.com>
2024-04-13 10:51:40 +0530

    changed footer

--- script/index.js
+++ script/index.js
@@ -1,16 +1,20 @@
 
-  var mainSwiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-  });
+var mainSwiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+      delay: 5000, // Time in milliseconds between slides
+      disableOnInteraction: false, // Set to true to stop autoplay when user interacts with the slider
+  }
+});
 
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
