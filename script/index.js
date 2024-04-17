b9fba97497248f190ffa11867d9c777f4f0c29e6
Muhammed faiz kk <fayizkk5@gmail.com>
2024-04-17 10:28:07 +0530

    first commit

--- script/index.js
+++ script/index.js
@@ -1,19 +1,14 @@
 
-var mainSwiper = new Swiper('.swiper', {
+var mainSwiper = new Swiper('.mainSwiper', {
   slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
       el: '.swiper-pagination',
       clickable: true,
   },
-  navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-  },
   autoplay: {
-      delay: 5000, // Time in milliseconds between slides
-      disableOnInteraction: false, // Set to true to stop autoplay when user interacts with the slider
-  }
+      delay: 5000,
+      disableOnInteraction: false,}
 });
 
   var swiper = new Swiper(".mySwiper", {
